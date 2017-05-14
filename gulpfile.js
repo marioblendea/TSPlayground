c6d1dca6b20f113de895e1712f43e8d37bd65d19
Mario Blendea <mblendea@microsoft.com>
2017-05-13 17:41:57 -0700

    more gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
 let sourcemaps = require('gulp-sourcemaps');
+let mocha = require('gulp-mocha');
 
 let tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', ['ts'], () => {
+gulp.task('default', ['ts', 'test'], () => {
   console.log('default');
 });
 
@@ -17,3 +18,11 @@ gulp.task('ts', () => {
     pipe(sourcemaps.write()).
     pipe(gulp.dest('bin'));
 })
+
+gulp.task('test', () => {
+  gulp.src('bin/**/*.spec.js', {read: false}).pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watch', ['ts', 'test'], () => {
+  gulp.watch('src/**/*.ts', ['ts', 'test']);
+});
