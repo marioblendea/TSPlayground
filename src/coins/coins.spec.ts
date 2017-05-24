9f704d8f6f7229673f6bc2ee3081b00f7da3944a
Mario <mario@Marios-MacBook-Pro.local>
2017-05-23 20:26:33 -0700

    coins using DP

--- src/coins/coins.spec.ts
+++ src/coins/coins.spec.ts
@@ -7,20 +7,28 @@ describe("Coins", () => {
   });
 
   it("should return 0 on invalid data", () => {
-    expect(Coins.GetCoins(5, [])).to.be.equal(0);
-    expect(Coins.GetCoins(-1, [1, 2, 3])).to.be.equal(0);
-    expect(Coins.GetCoins(5, [3, 2, -1])).to.be.equal(0);
+    expect(Coins.GetCoinsBacktracking(5, [])).to.be.equal(0);
+    expect(Coins.GetCoinsBacktracking(-1, [1, 2, 3])).to.be.equal(0);
+    expect(Coins.GetCoinsBacktracking(5, [3, 2, -1])).to.be.equal(0);
   });
 
   it("Should work when there is only one coin", () => {
-    const coins: number = Coins.GetCoins(5, [5]);
+    const coins: number = Coins.GetCoinsBacktracking(5, [5]);
     expect(coins).to.be.equal(1);
   });
 
-  it("Should work ", () => {
-    expect(Coins.GetCoins(5, [3, 2, 1])).to.be.equal(5);
-    expect(Coins.GetCoins(5, [5, 2, 1])).to.be.equal(4);
-    expect(Coins.GetCoins(3, [2])).to.be.equal(0);
+  it("Should return the number of ways a sum can be payed with coins of given denominations ", () => {
+    expect(Coins.GetCoinsBacktracking(5, [3, 2, 1])).to.be.equal(5);
+    expect(Coins.GetCoinsBacktracking(5, [5, 2, 1])).to.be.equal(4);
+    expect(Coins.GetCoinsBacktracking(3, [2])).to.be.equal(0);
+  });
+
+  it("Should return the number of ways a sum can be payed with coins of given denominations using DP ", () => {
+    expect(Coins.getCoinsDP(1, [1])).to.be.equal(1);
+    expect(Coins.getCoinsDP(2, [1])).to.be.equal(1);
+    expect(Coins.getCoinsDP(5, [1, 2, 3])).to.be.equal(5);
+    expect(Coins.getCoinsDP(5, [1, 2, 5])).to.be.equal(4);
+    expect(Coins.getCoinsDP(3, [2])).to.be.equal(0);
   });
 
 });
--- src/coins/coins.ts
+++ src/coins/coins.ts
@@ -1,5 +1,6 @@
 export class Coins {
-  public static GetCoins(sum: number, coins: number[]): number {
+  public static GetCoinsBacktracking(sum: number, coins: number[]): number {
+    // coins is expected to be sorted big to small
     if (sum <= 0) {
       return 0;
     }
@@ -36,6 +37,31 @@ export class Coins {
       currentCoinIdx = pop.coinIndex + 1;
     }
     return resultVal;
+  }
+
+  public static getCoinsDP(sum: number, coins: number[]): number
+  {
+    // coins is expected to be sorted small to big
+    // dp[i][j] -  how many ways you can make sum i using coins up to [0..j-1]
+    let dp: number[][] = new Array(sum + 1);
+    for (let i = 0; i <= sum; i++) {
+      dp[i] = new Array(coins.length + 1);
+      dp[i][0] = 0; // there are no ways to make any sum with no coins 
+      dp[i][1] = i % coins[0] === 0 ? 1 : 0; // if sum is divisible by the value of the first coin, there is 1 way to make the sum using just that coin; if not there are no ways
+    }
+
+    // sum 0 can be made with any coin in just 1 way; this is a sentinel to simplify logic in the main loop
+    for(let j=0;j<=coins.length;j++) {
+      dp[0][j] = 1;
+    }
+
+    for (let i = 1; i <= sum; i++) {
+      for (let j = 2; j <=  coins.length; j++) {
+        // to make sum i using coins from 0 to j, is the sum of how many ways you can make i using coins from 0 to j-1 + how many ways you can make the sum i-value of the j coin using coins from 0 to j
+        dp[i][j] = dp[i][j-1] + (i - coins[j-1] >= 0 ? dp[i-coins[j-1]][j] : 0);
+      }
+    }
+    return dp[sum][coins.length];
   }
 }
 
