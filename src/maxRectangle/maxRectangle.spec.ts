cdc6374dcbb746509ffec6acbc2997b265cc7f4c
Mario Blendea <mblendea@microsoft.com>
2018-04-20 20:29:20 +0000

    Updated maxRectangle.spec.ts

--- src/maxRectangle/maxRectangle.spec.ts
+++ src/maxRectangle/maxRectangle.spec.ts
@@ -3,6 +3,13 @@
 import { expect } from 'chai';
 import { MaxRectangle } from './maxRectangle';
 
+// given an array of numbers representing the height at each point, find out the maximum size rectangle that can be formed in that structure. For example, 2,1, 3 means something like this:
+//
+//
+// 3    |-|
+// 2|-| | |
+// 1| |-| |
+// 0-----------
 describe("Maximum size rectangle", () => {
   it("should return the max size", () => {
     expect(MaxRectangle.getMaxRectangle([10])).to.be.equal(10);
